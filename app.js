c7eb62b3ce1db123ec672a4d4258c8389cbde0a2
pedromartinrodriguez <pedromrodriguez@gmail.com>
2021-07-14 22:26:34 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+
+
+app.get('/home', (req, res)=>{
+    //res.send('Ruta ppal');
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+app.get('/register', (req, res)=>{
+    //res.send('Ruta ppal');
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+app.get('/login', (req, res)=>{
+    //res.send('Ruta ppal');
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
+
+//mencionar que public es la carpeta estatica de la aplicacion
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || '3000', () =>{
+    console.log('Corriendo puerto 3000');
+})
+
